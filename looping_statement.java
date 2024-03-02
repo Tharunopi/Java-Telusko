bf8352508e495809d9b6cf31b012f74b04da3802
Tharun Atithya <tharunaadhi6@gmail.com>
2024-03-02 10:41:38 +0530

    last

--- looping_statement.java
+++ looping_statement.java
@@ -1,5 +1,9 @@
 public class looping_statement {
     public static void main(String[] args) {
-        
+        int n = 1;                                         //while loop 
+        while(n<=100){
+            System.out.println("hi " + n);
+            n++;
+        }
     }
 }
