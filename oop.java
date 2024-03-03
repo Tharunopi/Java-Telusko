6c73de2b79e1e35081c8e5a535e355d001ff7ac4
Tharun Atithya <tharunaadhi6@gmail.com>
2024-03-03 22:07:31 +0530

    last

--- oop.java
+++ oop.java
@@ -1,3 +1,28 @@
+
+class calc{
+    int i = 1;
+    public int add(int a){
+        if(a<=10){
+            for(;i<=10;i++){
+                System.out.println("good morning "+i);
+            }
+        }
+        return 0;
+    }
+}
+
+
+
+
+
+
 public class oop {
-    
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 10;
+        // int result = a + b;
+        calc var = new calc();
+        
+        var.add(a);
+    }
 }
